cfeb0e7bb2c6718b7e1f02dc50ea96f0b4fd0ad5
Yohannes Wijaya <readyforservice@icloud.com>
2016-08-05 14:51:12 +0700

    Setup the custom class and the scenes for table view cell & establish connections to IBOutlets. Setup the custom class as well for the table view controller. Embed navigation controller into the table view controller.

---
+++ Notes/NotesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  NotesTableViewCell.swift
+//  Notes
+//
+//  Created by Yohannes Wijaya on 8/5/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewCell: UITableViewCell {
+    
+    // MARK: IBOutlet Properties
+    
+    @IBOutlet weak var noteLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
