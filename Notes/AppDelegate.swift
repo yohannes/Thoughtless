414cba1d05e04de1327bf9741600c6353a05cfc9
Yohannes Wijaya <readyforservice@icloud.com>
2016-08-31 15:09:29 +0700

    Fix the issue where new note controller is created and consequently presented to user while viewing an existing note after the app becomes active again. This fix will only present new note to user when user is not viewing existing note.

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -34,8 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
-        guard let validNavigationControllerEmbeddedInNotesViewController = self.window?.rootViewController?.storyboard?.instantiateViewController(withIdentifier: "NavigationControllerEmbeddedInNotesViewController") as? UINavigationController else { return }
-        self.window?.rootViewController?.present(validNavigationControllerEmbeddedInNotesViewController, animated: true, completion: nil)
+        guard let validRootViewController = self.window?.rootViewController,
+            validRootViewController.childViewControllers.count <= 1,
+            let validNavigationControllerEmbeddedInNotesViewController = validRootViewController.storyboard?.instantiateViewController(withIdentifier: "NavigationControllerEmbeddedInNotesViewController") as? UINavigationController else { return }
+        validRootViewController.present(validNavigationControllerEmbeddedInNotesViewController, animated: true, completion: nil)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
