57da7144d1d0ee5fb6e6b1c296f3134476129628
Yohannes Wijaya <readyforservice@icloud.com>
2016-09-27 15:28:49 +0700

    Implemented 3rd party library IQKeyboardManager to handle presentation between text view and keyboard.

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -10,16 +10,24 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        
-        guard let validRootViewController = self.window?.rootViewController,
-            validRootViewController.childViewControllers.count <= 1,
-            let validNavigationControllerEmbeddedInNotesViewController = validRootViewController.storyboard?.instantiateViewController(withIdentifier: "NavigationControllerEmbeddedInNotesViewController") as? UINavigationController else { return }
-        validRootViewController.present(validNavigationControllerEmbeddedInNotesViewController, animated: true, completion: nil)
-    }
+  
+  // MARK: - Stored Properties
+  
+  var window: UIWindow?
+  
+  // MARK: - UIApplicationDelegate Methods
+  
+  func applicationDidFinishLaunching(_ application: UIApplication) {
+    IQKeyboardManager.sharedManager().enable = true
+  }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    guard let validRootViewController = self.window?.rootViewController,
+      validRootViewController.childViewControllers.count <= 1,
+      let validNavigationControllerEmbeddedInNotesViewController = validRootViewController.storyboard?.instantiateViewController(withIdentifier: "NavigationControllerEmbeddedInNotesViewController") as? UINavigationController else { return }
+    validRootViewController.present(validNavigationControllerEmbeddedInNotesViewController, animated: true, completion: nil)
+  }
 }
 
