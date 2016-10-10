7dadebdf73015d49d65bebc9acb6cc72779655b7
Yohannes Wijaya <readyforservice@icloud.com>
2016-10-10 13:02:28 +0700

    - Fixed a bug where grey rectangle appears briefly on navigation bar during transition to preview markdown. - Changed "power up your note" from UIBarButtonItem to UILabel.

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   // MARK: - UIApplicationDelegate Methods
   
   func applicationDidFinishLaunching(_ application: UIApplication) {
+    self.window?.backgroundColor = UIColor.white
     IQKeyboardManager.sharedManager().enable = true
   }
   
