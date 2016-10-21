e34101e39448b0733942a930a022fa4a1722ba87
Yohannes Wijaya <readyforservice@icloud.com>
2016-10-21 12:26:48 +0800

    Refactored the code found in previous commit "Fix parsing of markdown's heading elements"

--- Notes/MarkdownKit/Classes/Elements/MarkdownHeader.swift
+++ Notes/MarkdownKit/Classes/Elements/MarkdownHeader.swift
@@ -11,6 +11,16 @@ import UIKit
 open class MarkdownHeader: MarkdownLevelElement {
 
   fileprivate static let regex = "^(#{1,%@})\\s*(.+)$"
+  
+  fileprivate enum MarkdownHeadingElements: CGFloat {
+    case oneHash = 6,
+    twoHashes = 4,
+    threeHashes = 2,
+    fourHashes = 0,
+    fiveHashes = -2,
+    sixHashes = -4,
+    zeroHash = -6
+  }
 
   open var maxLevel: Int
   open var font: UIFont?
@@ -37,30 +47,30 @@ open class MarkdownHeader: MarkdownLevelElement {
   open func attributesForLevel(_ level: Int) -> [String: AnyObject] {
     var attributes = self.attributes
     if let font = font {
-      var actualLevel: CGFloat = 0
+      var actualLevel: CGFloat
       if case 0 = level {
-        actualLevel = CGFloat(level) + 6
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.oneHash.rawValue
       }
       else if case 1 = level {
-        actualLevel = CGFloat(level) + 4
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.twoHashes.rawValue
       }
       else if case 2 = level {
-        actualLevel = CGFloat(level) + 2
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.threeHashes.rawValue
       }
       else if case 3 = level {
-        actualLevel = CGFloat(level) + 0
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.fourHashes.rawValue
       }
       else if case 4 = level {
-        actualLevel = CGFloat(level) - 2
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.fiveHashes.rawValue
       }
       else if case 5 = level {
-        actualLevel = CGFloat(level) - 4
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.sixHashes.rawValue
       }
       else if case 6 = level {
-        actualLevel = CGFloat(level) - 6
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.zeroHash.rawValue
       }
       else {
-        actualLevel = 0
+        actualLevel = MarkdownHeadingElements.fourHashes.rawValue
       }
 
       let headerFontSize: CGFloat = font.pointSize + (actualLevel * CGFloat(fontIncrease))
