2019260f85dc2bf73cad517326235565f57dae21
Yohannes Wijaya <readyforservice@icloud.com>
2016-10-22 10:58:59 +0800

    Improved the default first note.

--- Notes/NotesTableViewController.swift
+++ Notes/NotesTableViewController.swift
@@ -34,7 +34,7 @@ class NotesTableViewController: UITableViewController {
   // MARK: - Helper Methods
   
   func loadSampleNotes() {
-    guard let firstNote = Notes(entry: "Tap me to view this note.\n\nWhen you write word like **this** or *this* or even create a [link](http://apple.com), you just power up your note.\n\nTap 'Go!' to preview your enhanced note.\n\nTap 'How?' to learn more.") else { return }
+    guard let firstNote = Notes(entry: "Hello Sunshine! Come & tap me first!\n\n👇👇👇\n\nYou can power up your note by writing your **words** _like_ `these`, create an [url link](http://apple.com), or even make a todo list:\n\n* Watch WWDC videos.\n* Write code.\n* Fetch my girlfriend for a ride.\n* Write code.\n\nTap *Go!* to preview your enhanced note.\n\nTap *How?* to learn more.") else { return }
     guard let secondNote = Notes(entry: "Swipe me left or tap edit to delete.") else { return }
     guard let thirdNote = Notes(entry: "Tap edit to move me or delete me.") else { return }
     self.notes += [firstNote, secondNote, thirdNote]
