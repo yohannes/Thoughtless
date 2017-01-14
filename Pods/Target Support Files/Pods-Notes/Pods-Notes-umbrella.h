fcab944d3f808f65ed2dfc1c54e89d5e7bd48eef
Yohannes Wijaya <readyforservice@icloud.com>
2017-01-14 15:36:01 +0700

    Deintegrate Pod files

--- Pods/Target Support Files/Pods-Notes/Pods-Notes-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_NotesVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_NotesVersionString[];
-
--- Pods/Target Support Files/SwiftHEXColors/SwiftHEXColors-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SwiftHEXColors.h"
-
-FOUNDATION_EXPORT double SwiftHEXColorsVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftHEXColorsVersionString[];
-
