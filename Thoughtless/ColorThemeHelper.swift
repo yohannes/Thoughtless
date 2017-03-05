95ab23a3c6ec640ddcd76e68c9a021e7a64f545a
Yohannes Wijaya <readyforservice@icloud.com>
2017-03-05 15:52:28 +0700

    Added new charcoal color theme and changed the gray color value.

--- Thoughtless/ColorThemeHelper.swift
+++ Thoughtless/ColorThemeHelper.swift
@@ -10,10 +10,14 @@ import UIKit
 
 struct ColorThemeHelper {
     static func reederGray() -> UIColor {
-        return UIColor(hex: 0x3A3A37)!
+        return UIColor(hex: 0x4B4A47)!
     }
     
     static func reederCream() -> UIColor {
         return UIColor(hex: 0xE1E0D9)!
+    }
+    
+    static func reederCharcoal() -> UIColor {
+        return UIColor(hexString: "#43423F")!
     }
 }
